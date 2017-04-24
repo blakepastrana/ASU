ed31936db82e56e940b2b06fcde6827805d20888
bepastrana <blake.pastrana@accenturefederal.com>
2017-04-24 09:40:56 -0400

    Initial Spring Project Set-up

---
+++ src/main/java/afs/tas/AsuApplication.java
@@ -0,0 +1,12 @@
+package afs.tas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsuApplication.class, args);
+	}
+}
